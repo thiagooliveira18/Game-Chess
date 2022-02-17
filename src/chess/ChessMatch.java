da4341691568929190223d4981263b4083192a09
Thiago Oliveira <thiago.oliveira.ds18@gmail.com>
2022-02-17 17:01:32 -0300

    Possiveis movimentos para a peca

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(pos)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(!board.piece(pos).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece.");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
